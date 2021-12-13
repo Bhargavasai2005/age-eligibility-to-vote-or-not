5df88ebba8e98361bcb110879a93121d9d7c29ba
Bhargavasai2005 <94349388+Bhargavasai2005@users.noreply.github.com>
2021-12-13 17:34:16 +0530

    Add files via upload

---
+++ c++ of eligle to vote or  not.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age;
+	cout<<"enter age of the user:";
+	cin>>age;
+	
+	if(age>=18);
+	{
+		cout<<"\n you are eligible";
+	}
+	else
+	cout<<"\n you are not eligible"
+	return 0;
+}
